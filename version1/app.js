fd321d37c4738ba823743cf71792ce6fe68741ef
anshika-V <vishwakarma.1@iitj.ac.in>
2020-05-25 15:19:34 +0530

    basic setup of backend

---
+++ version1/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+var app = express();
+
+app.use(express.static("views"));
+app.set("view engine","ejs");
+//for home page
+app.get("/",function(req,res){
+    res.render("index");
+})
+
+
+app.listen(8080,function(){
+    console.log("Running at localhost:8080");
+})
