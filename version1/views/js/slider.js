b6292860e74e98de1c248f42202b3ea888acfb97
codernk01 <43872850+codernk01@users.noreply.github.com>
2020-05-27 19:31:17 +0530

    added how it works

--- version1/views/js/slider.js
+++ version1/views/js/slider.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $('.autoWidth').lightSlider({
-        autoWidth: true,
-        loop: true,
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:false,
         onSliderLoad: function() {
             $('.autoWidth').removeClass('cS-hidden');
         }
