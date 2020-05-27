1a20c8b1f9a6543c44288616dafcaefc98412260
codernk01 <43872850+codernk01@users.noreply.github.com>
2020-05-27 20:08:13 +0530

    bug fix

--- version1/views/js/slider.js
+++ version1/views/js/slider.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $('#autoWidth').lightSlider({
+    $('.autoWidth').lightSlider({
         autoWidth:true,
-        loop:false,
+        loop:true,
         onSliderLoad: function() {
             $('.autoWidth').removeClass('cS-hidden');
-        }
-    });
-});
+        } 
+    });  
+  });
