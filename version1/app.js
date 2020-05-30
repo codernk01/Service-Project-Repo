6ee77e7065743c684ef3b8470fe31512ca0677f7
Anshu <anshusahuiit@gmail.com>
2020-05-30 11:41:38 +0530

    Login/Register

--- version1/app.js
+++ version1/app.js
@@ -49,9 +49,9 @@ app.get("/",function(req,res){
 })
 
 //User routes
-app.get("/register",function(req,res){
-    res.render("register");
-})
+// app.get("/register",function(req,res){
+//     res.render("register");
+// })
 
 app.post("/register",function(req,res){
     var newUser= new User({
