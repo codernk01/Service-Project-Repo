141c4b54a302e4ec185cfe5928bc7101fe20525c
HARLEMSHAKE007 <behera.2@iitj.ac.in>
2020-06-11 10:10:46 +0530

    progressbar.html added work needs to be done

---
+++ version1/views/js/progressbar.js
@@ -0,0 +1,19 @@
+$(".st1").click(function() {
+    $(".profile").prop("checked", true);
+})
+
+$(".st2").click(function() {
+    $(".settings").prop("checked", true);
+})
+
+$(".st3").click(function() {
+    $(".posts").prop("checked", true);
+})
+
+$(".st4").click(function() {
+    $(".books").prop("checked", true);
+})
+
+$("ul li").click(function() {
+    $(this).addClass("active").siblings().removeClass("active");
+})
